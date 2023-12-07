756062627dab1b56486e1cdee7f4818ca246ceef
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2023-12-07 13:55:57 +0000

    Started connecting to xampp db

--- src/main.java
+++ src/main.java
@@ -1,5 +1,32 @@
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+
 public class main {
     public static void main(String[] args) {
+        //Video used to connect DB to Java App: https://youtu.be/AHFBPxWebFQ?si=UMsBsKxpAOizz-9S
+    }
 
+    String url="jdbc:mysql://localhost:3306/jdbcdemo";
+    String username="root";
+    String password="";
+
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select * from table");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + resultSet.getDouble(3));
+        }
+
+        connection.close();
+    }
+
+    catch (Exception e) {
+        System.out.println(e);
     }
 }
