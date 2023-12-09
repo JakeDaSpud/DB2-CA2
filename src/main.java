c9e7ccdfa41bad0d725c59a74803742062696080
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2023-12-09 00:18:59 +0000

    Added new code for connecting to db and executing SQL

--- src/main.java
+++ src/main.java
@@ -1,38 +1,53 @@
-import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 
 public class main {
     public static void main(String[] args) {
         //Video used to connect DB to Java App: https://youtu.be/AHFBPxWebFQ?si=UMsBsKxpAOizz-9S
 
-        //Xampp localhost URL for DB
-        String url="jdbc:mysql://localhost:3306/jdbcdemo";
+        Connection connection = null;
 
-        //Username details
-        String username="root";
-        String password="";
+        try{
+            //Xampp localhost URL for DB
+            String local_DB_URL = "jdbc:mysql://localhost:3306/KPop_Database_CA2";
 
-        //Error catching
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            //User Login Details
+            String username = "root";
+            String password = "";
 
-            Connection connection = DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(local_DB_URL, username, password);
 
+            //Testing
+            System.out.println("WHERE DOES THIS PRINT");
             Statement statement = connection.createStatement();
+            System.out.println("does it work or nah");
+            String sql = "INSERT INTO Company (CompanyName) VALUES ('JYP Entertainment');";
+            statement.executeUpdate(sql);
 
-            ResultSet resultSet = statement.executeQuery("select * from table");
+            //Using ResultSet to output query
+            ResultSet resultSet = statement.executeQuery("select * from Company");
 
-            //Constructing
+            //Constructing output
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(2 /* or 1 idk */));
+            }
+
+            // original resultSet while() loop
+            /*
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + resultSet.getDouble(3));
             }
+            */
 
+            //Closing / Ending connection
             connection.close();
         }
 
-        //Exception handling if error
-        catch (Exception e) {
-            System.out.println(e);
+        //Error Catching / Handling
+        catch (SQLException e) {
+            for(Throwable t: e)
+            {
+                t.printStackTrace();
+            }
         }
     }
 }
